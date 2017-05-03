d48640ba4153666e3dc506b6fc9e475ae80a6e8e
Gene Harvey <gharveymn@gmail.com>
2017-05-02 23:52:14 -0500

    version 0.0.1, working but probably p buggy

--- README.md
+++ README.md
@@ -1,65 +1,29 @@
-# nightswitch README
+# NightSwitch
 
-This is the README for your extension "nightswitch". After writing up a brief description, we recommend including the following sections.
+Automatically switches your VS Code theme between specified day and night themes.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Allows for automatic geolocation based on IP and manual GPS coordinate location.
 
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Geolocation is not turn on out of the box to protect privacy, however if you want to turn it on just invoke the setting `"nightswitch.geolocation": true`.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+None. Probably safer to set your main theme to either the day or night theme from the outset though.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+I'll do this in a bit... just look at package.json or your settings file for docs:
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+May have some synchronization issues. Haven't removed console logs yet.
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+Allows for GPS coordinate location, geolocation, manual input.
 
-### 1.0.0
+### 0.0.1
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release. Still probably pretty buggy (I wrote this pretty hastily).
--- src/extension.ts
+++ src/extension.ts
@@ -3,33 +3,165 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// get the current workbench configuration
-var wbconfig = vscode.workspace.getConfiguration('workbench')
-// get the user config for nightswitch
-var nsconfig = vscode.workspace.getConfiguration('nightswitch')
+var wbconfig = vscode.workspace.getConfiguration('workbench');
+var nsconfig = vscode.workspace.getConfiguration('nightswitch');
 
 export function activate(context: vscode.ExtensionContext) {
 
 	// // get the current workbench configuration
-	// var wbconfig = vscode.workspace.getConfiguration('workbench');
+	wbconfig = vscode.workspace.getConfiguration('workbench');
 	// // get the user config for nightswitch
-	// var nsconfig = vscode.workspace.getConfiguration('nightswitch');
+	nsconfig = vscode.workspace.getConfiguration('nightswitch');
 	var toggle = makeToggle();
 
 	//var time = new Date()
 	var time = new Date()
 	var SunCalc = require('suncalc')
 
+	const srisestr = nsconfig.get<string>('sunrise')
+	const ssetstr = nsconfig.get<string>('sunset')
+	console.log(srisestr)
+	console.log(ssetstr)
+	
+	var srisemanual = -1
+	var ssetmanual = -1
+	var srisetmrwmanual = -1
+	if (srisestr != null) {
+		srisemanual = parseManualTime(srisestr,time)
+		srisetmrwmanual = srisemanual+24*60*60*1000
+	}
+
+	if (ssetstr != null) {
+		ssetmanual = parseManualTime(ssetstr,time)
+	}
+
+	const manualTimes = [srisemanual,ssetmanual,srisetmrwmanual]
+	const forceSwitch = nsconfig.get<boolean>('forceSwitch')
+
+
 	if (nsconfig.get('geolocation')) {
-		//Nothing for now...
+		useGeo(SunCalc, manualTimes, forceSwitch)
 	}
 	else if (nsconfig.get('location') != null) {
 		console.log('NS: running location');
 		const coords = parseCoordinates(nsconfig.get<string>('location'))
-		console.log('(' + coords[0] + ',' + coords[1] + ')');
-		locationSwitch(coords, time, SunCalc)
+		if(Number.isNaN(coords[0]) || Number.isNaN(coords[1]))
+		{
+			vscode.window.showWarningMessage('Something went wrong with your coordinates. Try using the format \"(xxx.xxxx,yyy.yyyy)\".')
+		}
+		else{
+			console.log('NS: (' + coords[0] + ',' + coords[1] + ')');
+			locationSwitch(coords, time, SunCalc, manualTimes, forceSwitch)
+		}
 	}
 	console.info('NS: NightSwitch is now active!');
+}
+
+
+function parseManualTime(date: string, time: Date):number {
+    const hm = date.split(':')
+    const fullTime = time.getTime()
+    const currentHours = time.getHours()		  *60*60*1000
+    const currentMinutes = time.getMinutes()			*60*1000
+    const currentSeconds = time.getSeconds()			   *1000
+    const currentMilliseconds = time.getMilliseconds()
+
+    const todayStart = fullTime-currentHours-currentMinutes-currentSeconds-currentMilliseconds
+
+    const parsedTime = todayStart+(Number(hm[0])*60*60*1000)+(Number(hm[1])*60*1000)
+
+    return parsedTime
+}
+
+
+function useGeo(SunCalc: any, manualTimes: number[], forceSwitch: boolean) {
+	const getIp = require('get-ip');
+	getIp().then(
+		function (res) {
+			const ip = res[0];
+			const geoip = require('geoip-lite');
+			const geoCoords = geoip.lookup(ip).ll;
+			console.log('NS: geoCoords: (' + geoCoords[0] + ',' + geoCoords[1] + ')');
+			locationSwitch(geoCoords, new Date(), SunCalc, manualTimes, forceSwitch)
+		});
+}
+
+
+async function locationSwitch(coords: Number[], time: Date, SunCalc: any, 
+					manualTimes: number[], forceSwitch: boolean) {
+
+	var stimes = SunCalc.getTimes(time, coords[0], coords[1]);
+
+	const currtime = time.getTime()
+
+	var srise = manualTimes[0]
+	var sset = manualTimes[1]
+	var srisetmrw = manualTimes[2]
+	
+	if (srise=== -1)
+	{
+		srise = stimes.sunrise.getTime()
+		// set a virtual time 12hrs from now to get sunrise tomorrow
+		const virtualtime = currtime + 24 * 60 * 60 * 1000;
+		var stimestmrw = SunCalc.getTimes(new Date(virtualtime), coords[0], coords[1]);
+		srisetmrw = stimestmrw.sunrise.getTime()
+	}
+
+	if (sset === -1)
+	{
+		sset = stimes.sunset.getTime();
+	}
+
+	console.log('NS: current time: ' + currtime)
+	console.log('NS: sunrise: ' + srise)
+	console.log('NS: sunset: ' + sset)
+	console.log('NS: sunrise tomorrow: ' + srisetmrw)
+
+	await timeSwitch(currtime, srise, sset, srisetmrw, forceSwitch)
+	locationSwitch(coords, new Date(), SunCalc, manualTimes, forceSwitch)
+}
+
+
+async function timeSwitch(currtime: number, srise: number, sset: number, srisetmrw: number, forceSwitch: boolean) {
+	const timeToSunrise = srise - currtime,
+		timeToSunset = sset - currtime;
+
+	console.log('NS: timeToSunrise: ' + timeToSunrise)
+	console.log('NS: timeToSunset: ' + timeToSunset)
+
+	if (timeToSunrise > 0) {
+		// obviously give priority to sunrise
+
+		if (forceSwitch) {
+			setThemeNight()
+		}
+		console.log('NS: waiting until sunrise...')
+		await sleep(timeToSunrise)
+		setThemeDay()
+		console.log('NS: set theme to day')
+	}
+	else if (timeToSunset > 0) {
+		if (forceSwitch) {
+			setThemeDay()
+		}
+		console.log('waiting until sunset...')
+		await sleep(timeToSunset)
+		setThemeNight()
+		console.log('NS: set theme to night')
+	}
+	else {
+		// this means it's after sunset but before midnight
+
+		if (forceSwitch) {
+			setThemeNight()
+		}
+		console.log('NS: waiting until sunrise tomorrow...')
+		console.log('NS: sunrise tomorrow: ' + srisetmrw)
+		const timeToSunriseTmrw = srisetmrw - currtime
+		console.log('NS: timeToSunriseTmrw: ' + timeToSunriseTmrw)
+		await sleep(timeToSunriseTmrw)
+		setThemeDay()
+	}
 }
 
 
@@ -54,57 +186,8 @@ function makeToggle() {
 
 
 function parseCoordinates(coords: string): number[] {
-	const splcoords = coords.split(/\(|,|\)/);
-	return new Array(Number(splcoords[1]), Number(splcoords[2]))
-}
-
-
-async function locationSwitch(coords: Number[], time: Date, SunCalc: any) {
-	var stimes = SunCalc.getTimes(time, coords[0], coords[1]);
-
-	//Test
-	const currtime = time.getTime(),
-		srise = stimes.sunrise.getTime(),
-		sset = stimes.sunset.getTime();
-
-	console.log('NS: current time: ' + currtime)
-	console.log('NS: sunrise: ' + srise)
-	console.log('NS: sunset: ' + sset)
-
-	const timeToSunrise 	= srise - currtime,
-			timeToSunset 	= sset - currtime;
-
-	console.log('NS: timeToSunrise: ' + timeToSunrise)
-	console.log('NS: timeToSunset: ' + timeToSunset)
-
-	if (timeToSunrise > 0) {
-		// obviously give priority to sunrise
-		console.log('NS: waiting until sunrise...')
-		await sleep(timeToSunrise)
-		setThemeDay()
-		console.log('NS: set theme to day')
-	}
-	else if (timeToSunset > 0) {
-		console.log('waiting until sunset...')
-		await sleep(timeToSunset)
-		setThemeNight()
-		console.log('NS: set theme to night')
-	}
-	else {
-		// this means it's after sunset, but before midnight
-		console.log('NS: waiting until sunrise tomorrow...')
-		// set a virtual time 12hrs from now to get sunrise tomorrow
-		const virtualtime = currtime + 12*60*60*1000;
-		var stimes = SunCalc.getTimes(new Date(virtualtime), coords[0], coords[1]);
-		const srise = stimes.sunrise.getTime()
-
-		console.log('NS: sunrise tomorrow: ' + srise)
-		const timeToSunriseTmrw = srise - currtime
-		console.log('NS: timeToSunriseTmrw: ' + timeToSunriseTmrw)
-		await sleep(timeToSunriseTmrw)
-		setThemeDay()
-	}new Date()
-	locationSwitch(coords, new Date(), SunCalc)
+	const splcoords = coords.replace(/\(|\)/g,'').split(/,/);
+	return new Array(Number(splcoords[0]), Number(splcoords[1]))
 }
 
 
